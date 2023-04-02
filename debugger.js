575575fde14bf1dd300adda18584c47dc542c878
oolongstack <2245675739@qq.com>
2023-04-02 23:21:26 +0800

    feat: start

---
+++ debugger.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import path from "path";
+import { fileURLToPath } from "url";
+import rollup from "./lib/rollup.js";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const entry = path.resolve(__dirname, "src/main.js");
+const output = path.resolve(__dirname, "dist/bundle.js");
+
+rollup(entry, output);
