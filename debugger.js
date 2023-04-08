24e3cd6ce900c395e811d3b46de2e97546559b7b
oolongstack <2245675739@qq.com>
2023-04-08 11:41:10 +0800

    feat: write start

--- debugger.js
+++ debugger.js
@@ -5,6 +5,6 @@ import rollup from "./lib/rollup.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const entry = path.resolve(__dirname, "src/main.js");
-const output = path.resolve(__dirname, "dist/bundle.js");
+const output = path.resolve(__dirname, "./dist/bundle.js");
 
 rollup(entry, output);
